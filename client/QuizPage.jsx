19ea50ba10dd11599bd8cd8ebdb5f6469153fff1
Sakelig <71970061+Sakelig@users.noreply.github.com>
2022-03-12 00:01:12 +0100

    + client done

--- client/QuizPage.jsx
+++ client/QuizPage.jsx
@@ -5,8 +5,32 @@ import sound2 from "url:../soundData/E.M.M.I.Sound_Effects.mp3";
 import sound3 from "url:../soundData/pokemon_capture_sound_effect.mp3";
 import sound4 from "url:../soundData/BGM_EX4_Event_05.mp3";
 import sound5 from "url:../soundData/AmongUs_report_body.mp3";
+import sound6 from "url:../soundData/Mario_Yahoo_SoundEffect.mp3";
+import sound7 from "url:../soundData/pokemon-red_blue_yellow-item-found-sound-effect.mp3";
+import sound8 from "url:../soundData/lol_Welcome_Rift.mp3";
 
 export function QuizPage({ quizApi }) {
+  function testForInputNumber(number) {
+    switch (number) {
+      case 0:
+        return inputCollection.input1.toLowerCase();
+      case 1:
+        return inputCollection.input2.toLowerCase();
+      case 2:
+        return inputCollection.input3.toLowerCase();
+      case 3:
+        return inputCollection.input4.toLowerCase();
+      case 4:
+        return inputCollection.input5.toLowerCase();
+      case 5:
+        return inputCollection.input6.toLowerCase();
+      case 6:
+        return inputCollection.input7.toLowerCase();
+      case 7:
+        return inputCollection.input8.toLowerCase();
+    }
+  }
+
   let count = 0;
   const [soundList, setSoundList] = useState([
     sound1,
@@ -14,6 +38,9 @@ export function QuizPage({ quizApi }) {
     sound3,
     sound4,
     sound5,
+    sound6,
+    sound7,
+    sound8,
   ]);
   const [inputCollection, setInputCollection] = useState({
     input1: "",
@@ -24,10 +51,8 @@ export function QuizPage({ quizApi }) {
     input6: "",
     input7: "",
     input8: "",
-    input9: "",
   });
   const [answerCollection, setAnswerCollection] = useState([
-    false,
     false,
     false,
     false,
@@ -50,64 +75,21 @@ export function QuizPage({ quizApi }) {
   useEffect(() => {
     console.log("ping");
     if (listOfQuestions) {
-      for (let i = 0; i < listOfQuestions[0].answers.length; i++) {
-        if (
-          inputCollection.input1.toLowerCase() ===
-            listOfQuestions[0].answers[i].toLowerCase() &&
-          answerCollection[0] === false
-        ) {
-          setAnswerCollection(answerCollection, (answerCollection[0] = true));
-          setCount((prevState) => prevState + 1);
-          console.log("this happend again");
-          console.log(answerCollection[0]);
+      for (let i = 0; i < listOfQuestions.length; i++) {
+        for (let j = 0; j < listOfQuestions[i].answers.length; j++) {
+          if (
+            testForInputNumber(i) ===
+              listOfQuestions[i].answers[j].toLowerCase() &&
+            answerCollection[i] === false
+          ) {
+            console.log("test");
+            setAnswerCollection(answerCollection, (answerCollection[i] = true));
+            setCount((prevState) => prevState + 1);
+          }
         }
       }
 
-      for (let i = 0; i < listOfQuestions[1].answers.length; i++) {
-        if (
-          inputCollection.input2.toLowerCase() ===
-            listOfQuestions[1].answers[i].toLowerCase() &&
-          answerCollection[1] === false
-        ) {
-          setAnswerCollection(answerCollection, (answerCollection[1] = true));
-          setCount((prevState) => prevState + 1);
-        }
-      }
-
-      for (let i = 0; i < listOfQuestions[2].answers.length; i++) {
-        if (
-          inputCollection.input3.toLowerCase() ===
-            listOfQuestions[2].answers[i].toLowerCase() &&
-          answerCollection[2] === false
-        ) {
-          setAnswerCollection(answerCollection, (answerCollection[2] = true));
-          setCount((prevState) => prevState + 1);
-        }
-      }
-
-      for (let i = 0; i < listOfQuestions[3].answers.length; i++) {
-        if (
-          inputCollection.input4.toLowerCase() ===
-            listOfQuestions[3].answers[i].toLowerCase() &&
-          answerCollection[3] === false
-        ) {
-          setAnswerCollection(answerCollection, (answerCollection[3] = true));
-          setCount((prevState) => prevState + 1);
-        }
-      }
-
-      for (let i = 0; i < listOfQuestions[4].answers.length; i++) {
-        if (
-          inputCollection.input5.toLowerCase() ===
-            listOfQuestions[4].answers[i].toLowerCase() &&
-          answerCollection[4] === false
-        ) {
-          setAnswerCollection(answerCollection, (answerCollection[4] = true));
-          setCount((prevState) => prevState + 1);
-        }
-      }
-
-      console.log(answerCollection + "pong");
+      console.log(answerCollection + " pong");
     }
   }, [inputCollection]);
 
